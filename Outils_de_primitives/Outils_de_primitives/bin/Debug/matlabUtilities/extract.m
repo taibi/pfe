0f9866f2980c3674177222d3003a78dfb0c2908b
taibi <t.khachani@gmail.com>
2016-07-21 21:32:39 -0400

    import fichier comment

--- Outils_de_primitives/Outils_de_primitives/bin/Debug/matlabUtilities/extract.m
+++ Outils_de_primitives/Outils_de_primitives/bin/Debug/matlabUtilities/extract.m
@@ -1,19 +1,23 @@
-function  extract(source,destination,fichierdest,TypePrimitives,label)
+function  extract(source,destination,fichierdest,TypePrimitives,label,visage)
 obj = VideoReader(source);
 i = 1;
 tab = [];
+%mat = importdata(visage,'\n');
 cd(destination);
 fid = fopen(fichierdest,'w');
 nFrames = obj.NumberOfFrames;
-%for k = 1 : 3000 :nFrames
-for k = 1 : 10
+for k = 1 : nFrames
+%for k = 1 : 10
   fprintf(fid, '%s',label);
   fprintf(fid, ',%i', k);
   fprintf(fid, ',%f', obj.CurrentTime);
   this_frame = read(obj, k);
   imGray = rgb2gray(this_frame);
+  %imshow(imGray);
+  imGrayFaceBoxe = imGray(1 : 1 + 300 , 1 : 1 + 300);
+  %imshow(imGrayFaceBoxe);
   cd(TypePrimitives);
-  rep = lpq(imGray);
+  rep = lpq(imGrayFaceBoxe);
  
   fprintf(fid, ',%s', rep);
   fprintf(fid, '%s\n','');
